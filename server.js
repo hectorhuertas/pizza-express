14d69bd2ae2ea109b0a8db47269970807d639392
Hector Huertas <hectorhuertas@gmail.com>
2016-04-15 18:48:30 +0200

    Pizza show

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+const generateId = require('./lib/generate-id');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'jade');
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Pizza Express';
-app.set('view engine', 'jade');
+app.locals.pizzas = {};
 
 app.get('/', (request, response) => {
   response.render('index');
+});
+
+app.get('/pizzas/:id', (request, response) => {
+  var pizza = app.locals.pizzas[request.params.id];
+
+  response.render('pizza', { pizza: pizza });
+});
+
+app.post('/pizzas', (request, response) => {
+  var id = generateId();
+
+  app.locals.pizzas[id] = request.body;
+
+  response.sendStatus(201);
 });
 
 if (!module.parent) {
