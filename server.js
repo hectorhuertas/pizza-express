b4a58cedbd9d7002eabf0ce82b3c54789cc1b873
Hector Huertas <hectorhuertas@gmail.com>
2016-04-15 17:55:17 +0200

    Serve index.html

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Pizza Express';
+
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, '/static/index.html'));
+});
+
+if (!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+  });
+}
+
+module.exports = app;
